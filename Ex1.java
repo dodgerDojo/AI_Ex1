70dc5021bf2970150fb438b15a170a9e90939e4b
dan <dan@dan.com>
2016-12-03 12:31:48 +0200

    new project.

---
+++ Ex1.java
@@ -0,0 +1,38 @@
+package ex1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1
+{
+	private static void readFile(File fin) throws IOException {
+		FileInputStream fis = new FileInputStream(fin);
+	 
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+	 
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+	 
+		br.close();
+	}
+	public static void main(String[] args)
+	{
+		// wooho
+		File input_file = new File("C:\\dan\\AI\\HW\\HW1\\input.txt");
+		try
+		{
+			readFile(input_file);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
